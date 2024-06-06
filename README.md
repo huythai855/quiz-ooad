cb67923823814e6c2540564c569262575ecf34cb
qninhdt <qndt123@gmail.com>
2024-06-07 06:24:25 +0700

    update

--- README.md
+++ README.md
@@ -1,54 +1,10 @@
-# Quiz OOAD
-Quiz trắc nghiệm **OOAD** - **Phân tích thiết kế hướng đối tượng** cho người người nhà nhà cùng ôn. <br />
-Các câu hỏi được lấy từ bộ quiz OOAD của IBM, gồm 2 phần Analysis và Design. <br />
+# Quiz Image Processing
+Quiz trắc nghiệm **200** câu môn **Xử lý ảnh** cho người người nhà nhà cùng ôn.
+
+Các câu hỏi được lấy từ bộ ngân hàng câu hỏi được soạn bởi sinh viên **Vu Hoang Hanh**. 
+
 👉 Take the quiz now: [https://huythai855.github.io/quiz-ooad/](https://huythai855.github.io/quiz-ooad/)
-<br />
-<br />
 
-## Tự tạo bài quiz của mình để ôn tập môn khác
-Bạn có thể sử dụng template này để tự tạo bài quiz của để ôn tập các môn khác, với các bước khá đơn giản:
-1. [Fork](https://github.com/huythai855/quiz-ooad/fork) dự án này về.
-2. Thêm các dữ liệu câu hỏi của bạn vào file `questions.json`. Dữ liệu của bạn cần có dạng:
-   ```json
-    [
-      {
-          "question": "Question 1: Nội dung câu hỏi 1",
-          "answers": [
-              "A. Nội dung đáp án A",
-              "B. Nội dung đáp án B",
-              "C. Nội dung đáp án C",
-              "D. Nội dung đáp án D"
-          ],
-          "correct_answers": [
-              "C. Nội dung đáp án C"
-          ],
-          "image": "tên_file_ảnh_nếu_câu_đó_có_ảnh.png"
-      },
-      {
-          "question": "Question 2: Nội dung câu hỏi 2",
-          "answers": [
-              "A. Nội dung đáp án A",
-              "B. Nội dung đáp án B",
-              "C. Nội dung đáp án C",
-          ],
-          "correct_answers": [
-              "A. Nội dung đáp án A",
-              "B. Nội dung đáp án B",
-          ]
-      },
-   ]
-   ```
-   Tất nhiên, dữ liệu trên thực tế (đề cương thầy cô cho) không dưới dạng json và không hề dễ để convert thủ công sang dạng này. Khi đó, bạn có thể viết một prompt để ChatGPT làm hộ bạn công việc tay chân này, [tham khảo](https://chatgpt.com/share/09cab7e7-f564-4b8c-bf4a-f32d9fdbf3c1).
-3. Deploy lên web: Bạn vào mục Settings của repo -> Pages -> Chọn `deploy from a branch` -> chọn đúng branch và save. Github Actions sẽ lo phần còn lại cho bạn.
-4. Whohooo! You're done! Bạn sẽ thấy một thông báo `Your site is live at https://github-username-của-bạn.github.io/quiz-ooad/` thì có nghĩa bạn đã deploy thành công và có thể truy cập địa chỉ đó để làm quiz. Happy learning! 😇
+Thank **@huythai855** and **@hongquant.17** for the codebase.
 
-<br />
-
-## Demo
-<div style="width: 100%;">
-  <img src="https://i.imgur.com/nQJ1pvd.png" alt="GreenMart - Chợ nông sản sạch hàng đầu Việt Nam" style="width: 80%;">
-</div>
-
-
-
-_Built by @huythai855, @hongquant.17 and gpt-4o with luv!_ 💚
+> _Built by **@qninhdt** with ❤️_
--- script.js
+++ script.js
@@ -2,126 +2,99 @@ let questions = [];
 let usedQuestions = [];
 let currentQuestionIndex = -1;
 
-fetch('./questions.json')
-    .then(response => response.json())
-    .then(data => {
-        questions = data;
-        nextQuestion();
-    })
-    .catch(error => console.error('Error loading questions:', error));
+fetch("./questions.json")
+  .then((response) => response.json())
+  .then((data) => {
+    questions = data;
+    nextQuestion();
+  })
+  .catch((error) => console.error("Error loading questions:", error));
 
 function nextQuestion() {
-    if (usedQuestions.length === questions.length) {
-        usedQuestions = [];
-    }
-    
-    let newQuestionIndex;
-    do {
-        newQuestionIndex = Math.floor(Math.random() * questions.length);
-    } while (usedQuestions.includes(newQuestionIndex));
-    
-    currentQuestionIndex = newQuestionIndex;
-    usedQuestions.push(newQuestionIndex);
-    
-    const question = questions[newQuestionIndex];
-    displayQuestion(question);
+  if (usedQuestions.length === questions.length) {
+    usedQuestions = [];
+  }
+
+  let newQuestionIndex;
+  do {
+    newQuestionIndex = Math.floor(Math.random() * questions.length);
+  } while (usedQuestions.includes(newQuestionIndex));
+
+  currentQuestionIndex = newQuestionIndex;
+  usedQuestions.push(newQuestionIndex);
+
+  const question = questions[newQuestionIndex];
+  displayQuestion(question);
 }
 
 function displayQuestion(question) {
-    const questionElement = document.getElementById('question');
-    const questionImageElement = document.getElementById('question-image');
-    const answersContainer = document.getElementById('answers-container');
-    const resultElement = document.getElementById('result');
-    const statusElement = document.getElementById('status');
-    const explanationElement = document.getElementById('explanation');
+  const questionElement = document.getElementById("question");
+  const questionImageElement = document.getElementById("question-image");
+  const answersContainer = document.getElementById("answers-container");
+  const resultElement = document.getElementById("result");
+  const statusElement = document.getElementById("status");
+  const explanationElement = document.getElementById("explanation");
 
-    questionElement.textContent = question.question;
-    
-    if (question.image) {
-        questionImageElement.src = "./images/" + question.image;
-        questionImageElement.style.display = 'block';
-    } else {
-        questionImageElement.style.display = 'none';
-    }
+  questionElement.textContent = question.question;
 
-    answersContainer.innerHTML = '';
-    question.answers.forEach((answer, index) => {
-        const answerDiv = document.createElement('div');
-        answerDiv.className = 'answer';
+  if (question.image) {
+    questionImageElement.src = "./images/" + question.image;
+    questionImageElement.style.display = "block";
+  } else {
+    questionImageElement.style.display = "none";
+  }
 
-        const answerCheckbox = document.createElement('input');
-        answerCheckbox.type = 'checkbox';
-        answerCheckbox.id = `answer${index}`;
-        answerCheckbox.name = 'answer';
-        answerCheckbox.value = index + 1;
+  answersContainer.innerHTML = "";
+  question.answers.forEach((answer, index) => {
+    const answerDiv = document.createElement("div");
+    answerDiv.className = "answer";
 
-        const answerLabel = document.createElement('label');
-        answerLabel.htmlFor = `answer${index}`;
-        answerLabel.textContent = answer;
+    const answerCheckbox = document.createElement("input");
+    answerCheckbox.type = "radio";
+    answerCheckbox.id = `answer${index}`;
+    answerCheckbox.name = "answer";
+    answerCheckbox.value = index;
 
-        answerDiv.appendChild(answerCheckbox);
-        answerDiv.appendChild(answerLabel);
-        answersContainer.appendChild(answerDiv);
-    });
+    const answerLabel = document.createElement("label");
+    answerLabel.htmlFor = `answer${index}`;
+    answerLabel.textContent = answer;
 
-    resultElement.style.display = 'none';
-    statusElement.textContent = '';
-    explanationElement.textContent = '';
+    answerDiv.appendChild(answerCheckbox);
+    answerDiv.appendChild(answerLabel);
+    answersContainer.appendChild(answerDiv);
+  });
+
+  resultElement.style.display = "none";
+  statusElement.textContent = "";
+  explanationElement.textContent = "";
 }
 
 function submitQuiz() {
-    const selectedAnswers = Array.from(document.querySelectorAll('input[name="answer"]:checked')).map(cb => parseInt(cb.value));
-    const question = questions[currentQuestionIndex];
-    const correctAnswers = question.correct_answers.map(answer => parseInt(answer.split('.')[0]));
+  const selectedAnswer = Array.from(
+    document.querySelectorAll('input[name="answer"]:checked')
+  ).map((cb) => parseInt(cb.value))[0];
+  const question = questions[currentQuestionIndex];
+  const resultElement = document.getElementById("result");
+  const statusElement = document.getElementById("status");
+  const explanationElement = document.getElementById("explanation");
 
-    const statusElement = document.getElementById('status');
-    const explanationElement = document.getElementById('explanation');
+  // const selectedAnswers2 = Array.from(document.querySelectorAll('input[name="answer"]:checked')).map(cb => parseInt(cb.name.split('answer')[1]));
+  // const correctAnswers2 = question.correct_answers;
 
-    // const selectedAnswers2 = Array.from(document.querySelectorAll('input[name="answer"]:checked')).map(cb => parseInt(cb.name.split('answer')[1]));
-    // const correctAnswers2 = question.correct_answers;
+  const questionAnswers = question.answers;
+  const correctAnswer = question.correct_answer;
 
-    const questionAnswers = question.answers;
-    const correctAnswers2 = question.correct_answers;
+  resultElement.style.display = "block";
 
-    const selectedAnswersStr = selectedAnswers.map(answer => questionAnswers[answer - 1]);
-    // console.log('selectedAnswersStr:', selectedAnswersStr);
+  if (selectedAnswer === correctAnswer) {
+    statusElement.textContent = "Đúng";
+    statusElement.style.color = "green";
+  } else {
+    statusElement.textContent = "Sai";
+    statusElement.style.color = "red";
+  }
 
-    let trueCount = 0;
-    let falseCount = 0;
-
-    // console.log('questionAnswers:', questionAnswers);
-
-    for(let i = 0; i < questionAnswers.length; i++) {
-        const ans = questionAnswers[i];
-        if (selectedAnswersStr.includes(ans)) {
-            if (correctAnswers2.includes(ans)) {
-                trueCount++;
-            }
-            else {
-                falseCount++;
-            }
-        }
-        else {
-            if (correctAnswers2.includes(ans)) {
-                falseCount++;
-            }
-        }
-    }
-
-    if(trueCount === correctAnswers2.length && falseCount === 0) {
-        statusElement.textContent = 'Đúng';
-        statusElement.style.color = 'green';
-    }
-    else if(trueCount > 0) {
-        statusElement.textContent = 'Đúng một phần';
-        statusElement.style.color = 'orange';
-    }
-    else {
-        statusElement.textContent = 'Sai';
-        statusElement.style.color = 'red';
-    }
-
-
-    explanationElement.textContent = `Giải thích: ${question.correct_answers.join(', ')}`;
-    document.getElementById('result').style.display = 'block';
+  if (question.explanation) {
+    explanationElement.textContent = question.explanation;
+  }
 }
